4effbee7379dfe825be15d9669efd7fd372b138a
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-03-19 21:48:34 +0700

    sửa lại api tạo post: đẩy ảnh lên cloudinary

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,11 @@
 import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
+import { v2 as cloudinary } from "cloudinary";
+
 const createPost = async (req, res) => {
     try {
-        const { postedBy, text, img } = req.body;
+        const { postedBy, text } = req.body;
+        let { img } = req.body;
 
         if (!postedBy || !text) {
             return res
@@ -27,6 +30,12 @@ const createPost = async (req, res) => {
             return res.status(400).json({
                 error: `Text must be less than ${maxLength} characters`,
             });
+        }
+
+        // nếu có ảnh gửi lúc đăng post sẽ được đưa lên cloud, rồi cloud gửi url về
+        if (img) {
+            const uploadedResponse = await cloudinary.uploader.upload(img);
+            img = uploadedResponse.secure_url;
         }
 
         const newPost = new Post({ postedBy, text, img });
